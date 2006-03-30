d1c616f7616a0d04c2c2c1f48e27553a5ab79688
jon <jon@c7da345c-2a10-0410-88b2-338622cc0b2c>
2006-03-30 22:18:07 +0000

    adding challenge
    git-svn-id: svn://svn.jrock.us/blog_software@8 c7da345c-2a10-0410-88b2-338622cc0b2c

---
+++ lib/Blog/Challenge.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+# Challenge.pm - [description]
+# Copyright (c) 2006 Jonathan T. Rockway
+# $Id: $
+
+package Blog::Challenge;
+use Crypt::Random qw(makerandom);
+use DateTime;
+use YAML;
+
+use overload (q{==} => "equals", q{""} => "tostring");
+
+sub new {
+    my ($class, $args) = @_;
+    my $self = {};
+    
+    my $random = makerandom(Size => 128, Strength => 0);
+    my $now    = DateTime->now;
+ 
+    $self->{nonce} = "$random";
+    $self->{uri}   = $args->{uri} || die "specify URI";
+    $self->{date}  = "$now";
+    
+    bless $self, $class;
+}
+
+sub equals {
+    my $self = shift;
+    my $them = shift;
+
+    return 
+      ($self->{nonce} == $them->{nonce}) &&
+      ($self->{uri}   eq $them->{uri}  ) &&
+      ($self->{date}  eq $them->{date} ) ;
+}
+
+sub tostring {
+    my $self = shift;
+    return Dump($self);
+}
+
+1;
