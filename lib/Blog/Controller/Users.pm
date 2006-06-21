4a0212ab9f05e4a97090a7a2d9e8660c5eda9b1a
jon <jon@c7da345c-2a10-0410-88b2-338622cc0b2c>
2006-06-21 03:43:09 +0000

    some new templates
    git-svn-id: svn://svn.jrock.us/blog_software@16 c7da345c-2a10-0410-88b2-338622cc0b2c

---
+++ lib/Blog/Controller/Users.pm
@@ -0,0 +1,46 @@
+package Blog::Controller::Users;
+
+use strict;
+use warnings;
+use base 'Catalyst::Controller';
+
+=head1 NAME
+
+Blog::Controller::Users - Catalyst Controller
+
+=head1 SYNOPSIS
+
+See L<Blog>
+
+=head1 DESCRIPTION
+
+Provides a listing of users and information about those users.
+
+=head1 METHODS
+
+=cut
+
+sub default : Private {
+    my ( $self, $c ) = @_;
+
+    my @users = $c->model('UserStore')->users;
+    foreach my $user (@users){
+	$user->refresh;
+    }
+    $c->stash->{users} = [@users];
+    $c->stash->{template} = "users.tt";
+}
+
+
+=head1 AUTHOR
+
+Jonathan Rockway
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
