a15d53fcc49b68b12c37da6be2c9bd5efb0fd2fc
jon <jon@c7da345c-2a10-0410-88b2-338622cc0b2c>
2006-08-31 19:28:31 +0000

    added Item.pm back; repos is fixed, but I don't think this code works :)
    git-svn-id: svn://svn.jrock.us/blog_software@182 c7da345c-2a10-0410-88b2-338622cc0b2c

---
+++ trunk/Angerwhale/lib/Angerwhale/Model/Filesystem/Item.pm
@@ -0,0 +1,114 @@
+#!/usr/bin/perl
+# Item.pm - a Filesystem item (Article or Comment, usually)
+# Copyright (c) 2006 Jonathan Rockway
+
+package Angerwhale::Model::Filesystem::Item;
+use strict;
+use warnings;
+use Carp;
+
+use base qw(
+	    Class::Accessor
+	    Angerwhale::Model::Filesystem::Item::Components::Attributes
+	    Angerwhale::Model::Filesystem::Item::Components::Tags
+	    Angerwhale::Model::Filesystem::Item::Components::Comments
+	    Angerwhale::Model::Filesystem::Item::Components::Metadata
+	    Angerwhale::Model::Filesystem::Item::Components::Content
+	    Angerwhale::Model::Filesystem::Item::Components::GUID
+	    Angerwhale::Model::Filesystem::Item::Components::Signature
+	    Angerwhale::Model::Filesystem::Item::Components::Context
+	   ); 
+
+__PACKAGE__->mk_accessors(qw(base location parent));
+
+# make sort @articles sort by creation time
+use overload (q{<=>} => \&compare,
+	      q{cmp} => \&compare,
+	      # but still let other stuff work too	    
+	      fallback => "TRUE");
+
+=head1 NAME
+
+Angerwhale::Model::Filesystem::Item - a filesystem item that knows how
+to attach other C<Item>s to itself
+
+=head1 SYNOPSIS
+
+=head1 METHODS
+
+=head2 new( \%arguments )
+
+Creates a new Filesystem::Item; but you probably want
+C<Filesystem::Item::Article> or C<Filesystem::Item::Comment> instead.
+
+Arguments:
+
+=over 4
+
+=item base 
+
+[REQUIRED] The directory to read filesystem items from.
+
+=item path
+
+[REQUIRED] The file used to back this item.  Must exist, and must be a
+regular file.
+
+=back
+
+=cut
+
+sub new {
+    my ($class, $args) = @_;
+    
+    croak "Unexpected path: $args->{path}" if $args->{path};
+    my $base      = $args->{base};
+    my $location  = $args->{location};
+    my $parent    = $args->{parent};
+    
+    croak "$base is not a valid base directory" 
+      if(!defined $base || !-d $base || !-r $base);
+    croak "$location is not a valid path"     
+      if(!defined $location || -d $location);
+    
+    my $self = {};
+    bless $self, $class;
+    
+    $self->base($base);
+    $self->location($location);
+    # undefined if this is an article (as opposed to a comment)
+    $self->parent($parent);
+    
+    return $self->NEXT::new(@_);
+}
+
+=head2 name
+
+Returns the filename of this Item.
+
+=cut
+
+sub name {
+    my $self = shift;
+    my $name;
+
+    $self->location =~ m{([^/]+)$};
+    $name = $1;
+        
+    return $name;
+}
+
+=head2 compare
+
+Sorting function.  Sorts by reverse creation_time (newest first).
+
+=cut
+
+sub compare {
+    my $a = shift;
+    my $b = shift;
+
+    return $a->creation_time <=> $b->creation_time;
+}
+
+1;
