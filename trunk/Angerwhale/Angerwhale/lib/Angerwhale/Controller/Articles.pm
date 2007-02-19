901d914382ceefae3e6dafe99792b2bc86df99b3
jon <jon@c7da345c-2a10-0410-88b2-338622cc0b2c>
2007-02-19 15:40:02 +0000

    revert trunk to last stable revision
    git-svn-id: svn://svn.jrock.us/blog_software@556 c7da345c-2a10-0410-88b2-338622cc0b2c

---
+++ trunk/Angerwhale/Angerwhale/lib/Angerwhale/Controller/Articles.pm
@@ -0,0 +1,91 @@
+package Angerwhale::Controller::Articles;
+
+use strict;
+use warnings;
+use base 'Catalyst::Controller';
+use URI::Escape;
+
+=head1 NAME
+
+Angerwhale::Controller::Articles - Catalyst Controller
+
+=head1 SYNOPSIS
+
+See L<Angerwhale>
+
+Core article manager, for displaying archives, and blog pages.
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=head2 index
+
+Shows every article.
+
+=cut
+
+sub index : Private {
+    my ( $self, $c ) = @_;
+    
+    $c->stash->{page}     = 'article_list';
+    $c->stash->{template} = 'search_results.tt';
+    $c->stash->{title}    = 'Archives - ' . $c->config->{title};
+
+    my @articles = reverse sort $c->model('Filesystem')->get_articles();
+
+    $c->stash->{articles}      = [@articles];
+    $c->stash->{article_count} = scalar @articles;
+}
+
+=head2 single_article(['raw'])
+
+Displays a single article (with comments, etc.).  If 'raw' is passed
+as the argument, then the raw unformatted text is returned as an octet
+stream.
+
+=cut
+
+sub single_article : Path {
+    my ( $self, $c, @args ) = @_;
+    my $name = shift @args;
+    my $type = shift @args;
+
+    if ( !$name ) {
+        $c->detach('article_list');
+    }
+
+    $c->stash->{template} = 'article.tt';
+    eval { $c->stash->{article} = $c->model('Filesystem')->get_article($name); };
+    if ($@) {
+
+        # not found!
+        $c->stash->{template} = 'error.tt';
+        $c->response->status(404);
+        return;
+    }
+    $c->stash->{title} = $c->stash->{article}->title;
+
+    # if the user wants the raw message (to verify the signature),
+    # return that instead of rendering the template
+    if ( defined $type && $type eq 'raw' ) {
+        $c->response->content_type('application/octet-stream');
+        $c->response->body( $c->stash->{article}->raw_text(1) );
+        return;
+    }
+}
+
+=head1 AUTHOR
+
+Jonathan Rockway
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ trunk/Angerwhale/Angerwhale/lib/Angerwhale/Filter/Time.pm
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+# Time.pm
+# Copyright (c) 2006 Jonathan T. Rockway
+
+package Angerwhale::Filter::Time;
+use strict;
+use warnings;
+use Time::Duration qw(ago);
+use utf8;
+
+my @daynames = qw(日 月 火 水 木 金 土);
+
+# Historical note.  I am an idiot for using DateTime for this!!!
+
+=head1 Filter::Time
+
+Format times (in seconds past the epochs) from TT
+
+example:
+
+   [% SET seconds = 123 %]
+   [% seconds | time %]
+
+=head1 METHODS
+
+=head2 filter
+
+Does the actual conversion at template runtime.
+
+=cut
+
+# converts seconds past the epoch, localtime, to a pretty string
+sub filter {
+    my ( $class, $time, $args, $format ) = @_;
+    my @time  = localtime($time);
+
+    my $formatted = eval {
+        if($format){
+            require POSIX;
+            return POSIX::strftime($format, @time);
+        }
+    };
+    return $formatted if defined $formatted;
+      
+
+    my $year  = $time[5] + 1900;
+    my $month = $time[4] + 1;
+    my $day   = $time[3];
+    my $wkday = $time[6];
+
+    my $hour   = $time[2];
+    my $minute = $time[1];
+    my $sec = $time[0];
+
+    $minute = "0$minute" if $minute < 10;
+
+    my $ampm = ( $hour < 11 ) ? "am" : "pm";
+    $hour %= 12;
+    $hour =~ s/^0+$/12/;
+
+    $wkday = $daynames[ $wkday % 7 ];
+
+    #my $ago = time() - $time;
+    #if($ago < 86_400){
+    #	return ago($ago);
+    #}
+
+    return "$year-$month-$day ($wkday) at $hour:$minute $ampm";
+}
+
+1;
