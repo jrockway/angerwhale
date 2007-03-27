8714d59cc9cf73a0752d9eed248c5486dc4b3df5
Florian Ragwitz <rafl@debian.org>
2007-03-27 13:54:50 +0200

    Add Angerwhale::Content::Filter::Index which indexes all items using Plucene.

---
+++ lib/Angerwhale/Content/Filter/Index.pm
@@ -0,0 +1,54 @@
+# Copyright (c) 2007 Florian Ragwitz <rafl@debian.org>
+
+package Angerwhale::Content::Filter::Index;
+
+use strict;
+use warnings;
+use Path::Class;
+use Plucene::Document;
+use Plucene::Document::Field;
+use Plucene::Index::Writer;
+use Plucene::Analysis::SimpleAnalyzer;
+
+my $writer;
+my $index_dir = dir(Angerwhale->path_to(qw/root search_index/));
+
+sub filter {
+    $index_dir->rmtree;
+
+    my $analyzer = Plucene::Analysis::SimpleAnalyzer->new;
+
+    $writer = Plucene::Index::Writer->new(
+            $index_dir->stringify,
+            $analyzer,
+            1,
+    );
+
+    return bless sub {
+        my $self    = shift;
+        my $context = shift;
+        my $item    = shift;
+
+        my $content = $item->metadata->{formatted}->{text} || q{};
+
+        my $doc = Plucene::Document->new;
+        $doc->add( Plucene::Document::Field->Text(content => $content) );
+        $doc->add( Plucene::Document::Field->Text(title   => $item->metadata->{ title  }) );
+        $doc->add( Plucene::Document::Field->Text(author  => $item->metadata->{ author }->fullname) );
+
+        $doc->add( Plucene::Document::Field->UnIndexed(uri     => $item->metadata->{ uri     }) );
+        $doc->add( Plucene::Document::Field->UnIndexed(summary => $item->metadata->{ summary }) );
+
+        $writer->add_document($doc);
+        $writer->_flush;
+
+        return $item;
+    };
+}
+
+sub DESTROY {
+    undef $writer;
+    $index_dir->rmtree;
+}
+
+1;
